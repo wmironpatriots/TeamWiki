bcd6023aa0531688ad2ef0948359d644eed168aa
Dasun Abeykoon <Dasun20202020@gmail.com>
2025-06-29 01:25:18 -0700

    feat: create starting point for exercise
    Signed-off-by: Dasun Abeykoon <Dasun20202020@gmail.com>

---
+++ SectionB/src/main/java/org/frc6423/robot/subsystem/intake/Intake.java
@@ -0,0 +1,19 @@
+// Copyright (c) 2025 FRC 6423 - Ward Melville Iron Patriots
+// https://github.com/FIRSTTeam6423
+// 
+// Open Source Software; you can modify and/or share it under the terms of
+// MIT license file in the root directory of this project
+
+package org.frc6423.robot.subsystem.intake;
+
+import edu.wpi.first.wpilibj2.command.Subsystem;
+
+public class Intake implements Subsystem {
+  public static Intake create() {
+    return new Intake();
+  }
+
+  public Intake() {}
+
+  public void periodic() {}
+}
